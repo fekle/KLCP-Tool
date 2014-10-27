5796579bb96f673b2f1c1754d988b085ba40f60f
Felix Klein <me@felixklein.at>
2014-10-27 01:04:02 +0100

    Initial Commit

---
+++ bif-ws14-vsys-server/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bif-ws14-vsys-server
+//
+//  Created by Felix Klein on 27.10.14.
+//  Copyright (c) 2014 Aleksandar Lepojic, Felix Klein. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
